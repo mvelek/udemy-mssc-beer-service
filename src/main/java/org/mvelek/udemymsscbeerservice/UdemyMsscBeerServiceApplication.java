22745df2d597f89a10eabe7a7a72596b45670df1
Michal Velek <michal.velek@pricefx.eu>
2020-11-05 21:42:46 +0100

    - initial commit

---
+++ src/main/java/org/mvelek/udemymsscbeerservice/UdemyMsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.mvelek.udemymsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyMsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyMsscBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mvelek/udemymsscbeerservice/UdemyMsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mvelek.udemymsscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyMsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
